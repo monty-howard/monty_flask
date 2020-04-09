c8ec188d690c8eb2e2ca4f09f3d1ff520ef37e6c
dave-howard <fatdaveh@gmail.com>
2020-04-09 08:41:00 +0100

    Added local arg option to run on host 127.0.0.1 PEP8 standards fixes

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 from flask import Flask, url_for, redirect
+import sys
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "hello", 200
 
+
 @app.route('/monty')
 def monty():
     return redirect(url_for('static', filename='index.html'))
 
-app.run(host='0.0.0.0', port=80)
+
+if '--local' in sys.argv:
+    app.run(host='127.0.0.1', port=80)
+else:
+    app.run(host='0.0.0.0', port=80)
